101714a6a7f069d7e32ae4396649e7419e94d326
Sonja <sonja.ng1@gmail.com>
2022-02-28 12:17:26 -0500

    Edit About

--- components/About/About.js
+++ components/About/About.js
@@ -9,9 +9,9 @@ const About = () => {
             <h2>About</h2>
             <Div1>
                     <ImageDiv>
-                        <Image src={Portrait} layout="fill" objectFit="contain"/>
+                        <Image src={Portrait} layout="fill" alt="portrait" objectFit="contain"/>
                     </ImageDiv>
-                    <Paragraph>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, 
+                    <Paragraph>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, 
                         eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas
                         sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, 
                         qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.
