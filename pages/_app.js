da8af6760b9eba8e9d8ef8b3c6432b0f7671b1e4
Sonja <sonja.ng1@gmail.com>
2022-03-10 16:00:28 -0500

    Add framer motion

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,21 @@
 import Theme from "../styles/theme"
+import { motion } from 'framer-motion';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+    <motion.div initial="pageInitial" animate="pageAnimate" variants={{
+      pageInitial: {
+      opacity: 0
+      },
+      pageAnimate: {
+      opacity: 1
+     },
+    }}>
       <Theme>
         <Component {...pageProps} />
       </Theme>
+      </motion.div>   
     </>
   ) 
 }
